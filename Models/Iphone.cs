0968c8919074b5bbb7bcce136cf22b9078b1269f
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2024-01-02 17:04:55 +0000

    atual

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Chama o construtor da classe base
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na App Store do iPhone.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Criando instâncias dos smartphones
+        Iphone iphone = new Iphone("945938450", "iPhone SE", "IMEI8453", 64);
+        Nokia nokia = new Nokia("981093842", "Nokia 6", "IMEI9310", 64);
+
+        // Testando os métodos
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Telegram");
+
+        Console.ReadLine();
+    }
+}
